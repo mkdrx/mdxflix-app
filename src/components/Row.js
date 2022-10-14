41b2f157843f31605b53257a996b5af2a748ae24
Markus D <markusdrx@gmail.com>
2022-10-14 22:51:02 +0200

    finished project

---
+++ src/components/Row.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
+
+const Row = ({ title, fetchURL, rowID }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+
+  // Sliders
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  return (
+    <div>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relate flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + +rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {movies.map((item, id) => (
+            <Movie item={item} key={id} />
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Row;
---
+++ src/pages/Signup.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signUp(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="signup background"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[350px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+              <form
+                onSubmit={submitHandler}
+                className="w-full flex flex-col py-4"
+              >
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Sign Up
+                </button>
+                <div className="flex items-center justify-between text-sm text-gray-600">
+                  <p className="hover:text-white">
+                    <input className="mr-2" type="checkbox" />
+                    Remember me
+                  </p>
+                  <p className="hover:text-white cursor-pointer">Need Help?</p>
+                </div>
+                <p className="py-8">
+                  <span className="text-gray-600">
+                    Already subscribed to Mdxflix?
+                  </span>{" "}
+                  <Link to="/login">Sign In</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
